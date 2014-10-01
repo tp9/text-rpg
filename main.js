94f2272b0cdbddae8f5eccde890559e6d7b76cf0
Matthew Taylor <mstaveleytaylor@gmail.com>
2014-10-01 17:14:42 +0100

    Added a few things

---
+++ main.js
@@ -0,0 +1,8 @@
+// This is the main script. It won't ever contain a whole
+// lot of code, most of it is in the /assets/js/ folder.
+// This is where everything is assembled and most
+// importantly where the display object is linked to
+// the page.
+
+// Currently I am still working on setting things up here,
+// PLEASE DO NOT MAKE A PULL REQUEST YET.
